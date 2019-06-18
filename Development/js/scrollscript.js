19c9dede8363f2966b49628ee157bc5cc13487cd
Qendrim Vllasa <qendrimvllasa@hotmail.de>
2019-06-19 00:41:26 +0200

    changes on navigation

---
+++ Development/js/scrollscript.js
@@ -0,0 +1,29 @@
+window.onscroll = function() {scrollFunctionResize(), scrollFunctionOpacity()};
+
+
+function scrollFunctionResize() {
+    if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
+        document.getElementById("paddingNav").style.paddingTop = "0px";
+        document.getElementById("paddingNav").style.paddingBottom = "0px";
+
+
+
+    } else {
+        document.getElementById("paddingNav").style.paddingTop = "20px";
+        document.getElementById("paddingNav").style.paddingBottom = "20px";
+
+
+
+    }
+}
+
+function scrollFunctionOpacity () {
+    if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
+        document.getElementById("background-dark").style.background = "rgba(0,0,0 , 1)";
+
+    }
+    else {
+        document.getElementById("background-dark").style.background = "rgba(0,0,0 , 0.5)";
+
+    }
+}
