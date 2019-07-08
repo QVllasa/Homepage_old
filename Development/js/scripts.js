aa400670e016ffb68d661994fd7321b2c4032ac6
Qendrim Vllasa <qendrimvllasa@hotmail.de>
2019-07-08 22:46:16 +0200

    working on resume section

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -155,6 +155,7 @@ window.onload = function () {
 };
 
 //carousel-links
+/*
 var btns = document.getElementsByClassName('link-element');
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function () {
@@ -177,6 +178,7 @@ $("#upcoming-projects-link").click(function () {
 $("#archived-projects-link").click(function () {
   $('.portfolio-slideshow').carousel(2);
 });
+*/
 
 
 //circle progress bar
@@ -278,6 +280,51 @@ createNormalBars(nbar5, 0.9);
 createNormalBars(nbar6, 0.9);
 
 
-//canvas
+// init Isotope
+var $grid = $('.grid').isotope({
+  itemSelector: '.element-item',
+  layoutMode: 'fitRows',
+  getSortData: {
+    name: '.name',
+    symbol: '.symbol',
+    number: '.number parseInt',
+    category: '[data-category]',
+    weight: function (itemElem) {
+      var weight = $(itemElem).find('.weight').text();
+      return parseFloat(weight.replace(/[\(\)]/g, ''));
+    }
+  }
+});
 
+// filter functions
+var filterFns = {
+  // show if number is greater than 50
+  numberGreaterThan50: function () {
+    var number = $(this).find('.number').text();
+    return parseInt(number, 10) > 50;
+  },
+  // show if name ends with -ium
+  ium: function () {
+    var name = $(this).find('.name').text();
+    return name.match(/ium$/);
+  }
+};
+
+// bind filter button click
+$('#filters').on('click', '.link-element', function () {
+  var filterValue = $(this).attr('data-filter');
+  // use filterFn if matches value
+  filterValue = filterFns[filterValue] || filterValue;
+  $grid.isotope({filter: filterValue});
+});
+
+
+// change is-checked class on buttons
+$('.button-group').each(function (i, buttonGroup) {
+  var $buttonGroup = $(buttonGroup);
+  $buttonGroup.on('click', '.link-element', function () {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    $(this).addClass('is-checked');
+  });
+});
 
