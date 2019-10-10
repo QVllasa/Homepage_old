7ceaafcc5ccd0e72e97d177878235d11d6e6dbb8
Qendrim Vllasa <qendrimvllasa@hotmail.de>
2019-10-10 18:00:08 +0200

    working on backend

---
+++ Development/inc/db.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbname = "qendrimvllasa";
+$user = "qendrimvllasa";
+$password = "Dominim123_!";
+$dbhost = "116.202.107.98:3306";
+
+$pdo = new PDO("jdbc:mysql:host=$dbhost; dbname=$dbname"; $user; $password);
