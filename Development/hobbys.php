95fb33885712cdfb7b5f64cbaf3f955d04782d49
Qendrim Vllasa <qendrimvllasa@hotmail.de>
2019-10-14 00:10:30 +0200

    working on backend

--- Development/hobbys.php
+++ Development/hobbys.php
@@ -550,9 +550,9 @@
             <div class="row align-items-center pt-5">
                 <div class="container  ">
                     <div class="row justify-content-center">
-                        <a href="impressum.html">Impressum</a>
+                        <a href="impressum.php">Impressum</a>
                         <a>|</a>
-                        <a href="datenschutz.html">Datenschutz</a>
+                        <a href="datenschutz.php">Datenschutz</a>
                     </div>
                     <div class="row justify-content-center mt-1">
                         <p>&copy; Qendrim Vllasa 2019</p>
