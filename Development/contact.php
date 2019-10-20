e4734a6f2c69253ecdeafffb5f86dd43aefaca7f
Qendrim Vllasa <qendrimvllasa@hotmail.de>
2019-10-20 23:04:30 +0200

    working on backend

--- Development/contact.php
+++ Development/contact.php
@@ -1,6 +1,6 @@
 <?php require ("inc/db.inc.php");
 require ("inc/functions.inc.php");
-var_dump($pdo);
+
 
 $stmt =  $pdo -> prepare("INSERT INTO `messages` (`name`, `email`, `subject`, `text`) VALUE(:name, :email, :subject, :message)");
 
