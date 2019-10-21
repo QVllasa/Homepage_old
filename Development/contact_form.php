b79a5c364317aa94230c7b2d0ebe6a00f735e29c
Qendrim Vllasa <qendrimvllasa@hotmail.de>
2019-10-21 02:39:02 +0200

    working on backend

--- Development/contact_form.php
+++ Development/contact_form.php
@@ -1,32 +1,46 @@
 <?php
-
-//require ("inc/db.inc.php");
-require ("inc/functions.inc.php");
-
-print_r($_POST);
-
 $servername = "localhost";
 $username = "qendrimvllasa";
 $password = "Dominim123_!";
 $dbname = "qendrimvllasa";
 
+
+
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-
+  // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $sql = "INSERT INTO messages (name, email, subject, text)
-    VALUES ('John', 'sfs123fsdfsd@example.com', 'nachricht', 'hier kommt die nachricht' )";
 
-  $conn->exec($sql);
-} catch (PDOException $e) {
-  echo $sql . "<br>" . $e->getMessage();
+  // prepare sql and bind parameters
+  $stmt = $conn->prepare("INSERT INTO `messages` (`name`, `email`, `phone`, `subject`, `text`) 
+    VALUES (:name, :email, :phone, :subject, :text)");
+  $stmt->bindParam(':name', $name);
+  $stmt->bindParam(':email', $email);
+  $stmt->bindParam(':phone', $phone);
+  $stmt->bindParam(':subject', $subject);
+  $stmt->bindParam(':text', $text);
+
+
+  // insert a row
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $phone=$_POST['phone'];
+  $subject=$_POST['subject'];
+  $text=$_POST['message'];
+
+
+
+  $stmt->execute();
+
+
+  echo "New records created successfully";
 }
-
+catch(PDOException $e)
+{
+  echo "Error: " . $e->getMessage();
+}
 $conn = null;
-
-
 ?>
 
+<?php// require ("parts/contact.php");?>
 
-
-<?php require ("parts/contact.php");?>
