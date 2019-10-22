60c5fdcc7c9eb7fac6a247a8c30c695c62437852
Qendrim Vllasa <qendrimvllasa@hotmail.de>
2019-10-22 22:35:33 +0200

    preparing email

--- Development/test_mail2.php
+++ Development/test_mail2.php
@@ -1,33 +1,5 @@
 <?php
-$mailtext = '<html>
-<head>
-    <title>HTML-E-Mail mit PHP erstellen</title>
-</head>
- 
-<body>
- 
-<h1>HTML-E-Mail mit PHP erstellen</h1>
- 
-<p>Diese E-Mail wurde mit PHP und HTML erstellt</p>
- 
-<table border="1">
-  <tr>
-    <td>Beschreibung</td>
-    <td>Anzahl Seiten</td>
-  </tr>
-  <tr>
-    <td>PHP lernen mit PHP-Kurs.com</td>
-    <td>über 100</td>
-  </tr>
-</table>
- 
-<p>Die meisten HTML-Tags wie <b>fett</b>
-und <i>kursiv</i> stehen zur
-Verfügung</p>
- 
-</body>
-</html>
-';
+$mailtext = '';
 
 $empfaenger = "qendrimvllasa@hotmail.de"; //Mailadresse
 $absender   = "qendrim.vllasa@gmail.com";
